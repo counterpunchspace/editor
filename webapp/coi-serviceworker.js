e8b0425196339251d89fec6e6388f75a2ea54cf9
Yanone <post@yanone.de>
2025-12-12 11:26:24 +0100

    Release v0.1.12a

--- webapp/coi-serviceworker.js
+++ webapp/coi-serviceworker.js
@@ -3,7 +3,7 @@
 let coepCredentialless = false;
 
 // PWA Cache configuration
-const VERSION = 'v0.1.11a';
+const VERSION = 'v0.1.12a';
 const CACHE_NAME = 'contxt-pwa-' + VERSION;
 const CDN_CACHE_NAME = 'contxt-cdn-cache-' + VERSION;
 const OFFLINE_URL = '/index.html';
