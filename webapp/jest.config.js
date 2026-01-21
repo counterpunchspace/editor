8691f7965e105c172b66157f197547d5f9f8257b
Yanone <post@yanone.de>
2026-01-21 19:53:34 +0100

    Prettier

--- webapp/jest.config.js
+++ webapp/jest.config.js
@@ -7,7 +7,8 @@ module.exports = {
     testMatch: ['**/*.test.js'], // Only run .test.js files, exclude Playwright .spec.ts files
     moduleNameMapper: {
         // Map babelfont-ts to local vendor directory
-        '^babelfont-ts$': '<rootDir>/vendor/babelfont-rs/babelfont-ts/src/index.ts',
+        '^babelfont-ts$':
+            '<rootDir>/vendor/babelfont-rs/babelfont-ts/src/index.ts',
         // Mock WASM modules since they can't be loaded in Jest environment
         '^.+\\.wasm$': '<rootDir>/tests/__mocks__/wasmMock.js',
         '^../wasm-dist/babelfont_fontc_web$':
